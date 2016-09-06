a3bcd46cccf5e45928220ca270c2884cf4010011
gavinleng <gleng.email@gmail.com>
2016-09-06 10:21:20 +0100

    v1

--- bboxNeighbors-config.js
+++ bboxNeighbors-config.js
@@ -6,8 +6,8 @@
 "use strict";
 
 module.exports = {
-	"url_area": 'https://q.nqminds.com/v1/datasets/H1lUgEmRP/data?opts={"limit":450000}&filter={"properties.area_id":"W06000002"}',
+	"url_area": 'https://q.nqminds.com/v1/datasets/S1xZSFLVj/data?opts={"limit":450000}&filter={"properties.area_id":"E06000026"}',
 	"margin": 5000,//meter
-	"url_search": 'https://q.nqminds.com/v1/datasets/H1lUgEmRP/data?opts={"limit":450000}'
-	//"url_search": 'https://q.nqminds.com/v1/datasets/H1xJDR_kd/data?opts={"limit":450000}'
+	"url_search": 'https://q.nqminds.com/v1/datasets/S1xZSFLVj/data?opts={"limit":450000}'
+	//"url_search": 'https://q.nqminds.com/v1/datasets/r1g0NOU4s/data?opts={"limit":450000}'
 };
